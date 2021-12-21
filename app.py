51d53604b460aa6d66bc57b8c5397321f1ec18fb
Jonathan Lui <jonslui@gmail.com>
2021-12-21 14:09:24 -0500

    created a new app.py folder with /game/ POST request route

--- app.py
+++ app.py
@@ -1,226 +1,236 @@
-import os
-import struct
-import secrets
-
-from flask import Flask, request
+import os.path
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
-# ===== Setup =====
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+ \
-                os.path.join(basedir, 'db.sqlite3')
+app = Flask(__name__)
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, 'db.sqlite3')
+DB_URI = 'sqlite:///{}'.format(DB_PATH)
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-
-# ===== Database Models =====
-class User(db.Model):
-    # PK User_id auto created by SQLAlchemy
-    username = db.Column(db.String(100))
-    rio_key = db.Column(db.String(32), primary_key=True)
-
+# ===== Models =====
 class Game(db.Model):
-    game_id = db.Column(db.Integer, primary_key=True)
-    date_time = db.Column(db.String(255))
-    stadium_id = db.Column(db.Integer)
-    netplay = db.Column(db.Boolean)
-    ranked = db.Column(db.Integer)
-    
-    away_player_id = db.Column(db.ForeignKey('user.rio_key'), nullable=True) #One-to-One
-    home_player_id = db.Column(db.ForeignKey('user.rio_key'), nullable=True) #One-to-One
+  id = db.Column(db.String(255), primary_key = True)
+  date_time = db.Column(db.String(255))
+  ranked = db.Column(db.Integer)
+  stadium_id = db.Column(db.String(255))
+  # away_player_id = db.Column(db.ForeignKey('user.rio_key'), nullable=True) #One-to-One
+  # home_player_id = db.Column(db.ForeignKey('user.rio_key'), nullable=True) #One-to-One
+  away_score = db.Column(db.Integer)
+  home_score = db.Column(db.Integer)
+  innings_selected = db.Column(db.Integer)
+  innings_played = db.Column(db.Integer)
+  home_captain = db.Column(db.String(255))
+  home_roster_0 = db.Column(db.String(255))
+  home_roster_1 = db.Column(db.String(255))
+  home_roster_2 = db.Column(db.String(255))
+  home_roster_3 = db.Column(db.String(255))
+  home_roster_4 = db.Column(db.String(255))
+  home_roster_5 = db.Column(db.String(255))
+  home_roster_6 = db.Column(db.String(255))
+  home_roster_7 = db.Column(db.String(255))
+  home_roster_8 = db.Column(db.String(255))
+  away_captain = db.Column(db.String(255))
+  away_roster_0 = db.Column(db.String(255))
+  away_roster_1 = db.Column(db.String(255))
+  away_roster_2 = db.Column(db.String(255))
+  away_roster_3 = db.Column(db.String(255))
+  away_roster_4 = db.Column(db.String(255))
+  away_roster_5 = db.Column(db.String(255))
+  away_roster_6 = db.Column(db.String(255))
+  away_roster_7 = db.Column(db.String(255))
+  away_roster_8 = db.Column(db.String(255))
+  quitter = db.Column(db.Integer) #0=None, 1=Away, 2=Home
 
-    away_captain = db.Column(db.Integer)
-    home_captain = db.Column(db.Integer)
-
-    away_roster_0 = db.Column(db.Integer)
-    away_roster_1 = db.Column(db.Integer)
-    away_roster_2 = db.Column(db.Integer)
-    away_roster_3 = db.Column(db.Integer)
-    away_roster_4 = db.Column(db.Integer)
-    away_roster_5 = db.Column(db.Integer)
-    away_roster_6 = db.Column(db.Integer)
-    away_roster_7 = db.Column(db.Integer)
-    away_roster_8 = db.Column(db.Integer)
-
-    home_roster_0 = db.Column(db.Integer)
-    home_roster_1 = db.Column(db.Integer)
-    home_roster_2 = db.Column(db.Integer)
-    home_roster_3 = db.Column(db.Integer)
-    home_roster_4 = db.Column(db.Integer)
-    home_roster_5 = db.Column(db.Integer)
-    home_roster_6 = db.Column(db.Integer)
-    home_roster_7 = db.Column(db.Integer)
-    home_roster_8 = db.Column(db.Integer)
-
-    away_score = db.Column(db.Integer)
-    home_score = db.Column(db.Integer)
-
-    innings_selected = db.Column(db.Integer)
-    innings_played = db.Column(db.Integer)
-
-    quitter = db.Column(db.Integer) #0=None, 1=Away, 2=Home
-
-class GameCharacter(db.Model):
-    #GameCharacter_id auto created by SQLAlchemy
-    game_id = db.Column(db.Integer, db.ForeignKey('game.game_id'), nullable=False)
-    team_id = db.Column(db.Integer) #0=Away, 1=Home
-    roster_loc = db.Column(db.Integer) #0-8
-    char_id = db.Column(db.Integer) #TODO Convert to foreign key for character info table
-    superstar = db.Column(db.Boolean)
-
-    #Defensive stats
-    batters_faced = db.Column(db.Integer)
-    runs_allowed = db.Column(db.Integer)
-    batters_walked = db.Column(db.Integer)
-    batters_hit = db.Column(db.Integer)
-    hits_allowed = db.Column(db.Integer)
-    homeruns_allowed = db.Column(db.Integer)
-    pitches_thrown = db.Column(db.Integer)
-    stamina = db.Column(db.Integer)
-    was_pitcher = db.Column(db.Integer)
-    batter_outs = db.Column(db.Integer)
-    strike_outs_pitched = db.Column(db.Integer)
-    star_pitches_thrown = db.Column(db.Integer)
-    big_plays = db.Column(db.Integer)
-    #Rio curated stats
-    innings_pitched = db.Column(db.Integer)
-
-    #Offensive Stats
-    at_bats = db.Column(db.Integer)
-    hits = db.Column(db.Integer)
-    singles = db.Column(db.Integer)
-    doubles = db.Column(db.Integer)
-    triples = db.Column(db.Integer)
-    homeruns = db.Column(db.Integer)
-    strike_outs = db.Column(db.Integer)
-    walks_bb = db.Column(db.Integer)
-    walks_hit = db.Column(db.Integer)
-    rbi = db.Column(db.Integer)
-    bases_stolen = db.Column(db.Integer)
-    star_hits = db.Column(db.Integer)
-
-class PitchSummary(db.Model):
-    #PitchSummary_id auto created by SQLAlchemy   
-    batter_id = db.Column(db.Integer, db.ForeignKey('gamecharacter.id'))
-    inning = db.Column(db.Integer)
-    half_inning = db.Column(db.Integer)
-    batter_score = db.Column(db.Integer)
-    pitcher_score = db.Column(db.Integer)
-    balls = db.Column(db.Integer)
-    strikes = db.Column(db.Integer)
-    outs = db.Column(db.Integer)
-    runner_on_1 = db.Column(db.Integer)
-    runner_on_2 = db.Column(db.Integer)
-    runner_on_3 = db.Column(db.Integer)
-    chem_links_ob = db.Column(db.Integer)
-    star_chance = db.Column(db.Integer)
-    batter_stars = db.Column(db.Integer)
-    pitcher_stars = db.Column(db.Integer)
-    pitcher_id = db.Column(db.Integer, db.ForeignKey('gamecharacter.id'))
-    pitcher_handedness = db.Column(db.Integer)
-    pitch_type = db.Column(db.Integer)
-    charge_pitch_type = db.Column(db.Integer)
-    star_pitch = db.Column(db.Integer)
-    pitch_speed = db.Column(db.Integer)    
-    rbi = db.Column(db.Integer)
-    num_outs = db.Column(db.Integer)
-    result_inferred = db.Column(db.String(100))
-    result_game = db.Column(db.Integer)
-
-class ContactSummary(db.Model):
-    pitch_id = db.Column(db.Integer, db.ForeignKey('pitchsummary.id'))
-    type_of_contact = db.Column(db.Integer)
-    charge_power_up = db.Column(db.Float)
-    charge_power_down = db.Column(db.Float)
-    star_swing = db.Column(db.Integer)
-    star_swing_five_star = db.Column(db.Integer)
-    input_direction = db.Column(db.Integer)
-    batter_handedness = db.Column(db.Integer)
-    hit_by_pitch = db.Column(db.Integer)
-    ball_angle = db.Column(db.Integer)
-    ball_horiz_power = db.Column(db.Integer)
-    ball_vert_power = db.Column(db.Integer)
-    ball_x_velocity = db.Column(db.Float)
-    ball_y_velocity = db.Column(db.Float)
-    ball_z_velocity = db.Column(db.Float)
-    ball_x_pos = db.Column(db.Float)
-    ball_y_pos = db.Column(db.Float)
-    ball_z_pos = db.Column(db.Float)
-
-class FieldingSummary(db.Model):
-    contact_id = db.Column(db.Integer, db.ForeignKey('contactsummary.id'))
-    char_id = db.Column(db.Integer)
-    position = db.Column(db.Integer) #0-8. 0=P, 1=C, ... 8=RF
-
-class UserCharacterStats(db.Model):
-    user = db.Column(db.ForeignKey('user.rio_key'), nullable=False)
-    char_id = db.Column(db.Integer) # TODO change to FK to char table
-    superstar = db.Column(db.Boolean)
-    games = db.Column(db.Integer)
-    captained = db.Column(db.Integer)
-    at_bats = db.Column(db.Integer)
-    hits = db.Column(db.Integer)
-    walks = db.Column(db.Integer)
-    batting_avg = db.Column(db.Float) #Calculated
-    obp = db.Column(db.Float) #Calculated
-    slg = db.Column(db.Float) #Calculated
-    ops = db.Column(db.Float) #Calculated
-    rbi  = db.Column(db.Integer)
-    bases_stolen = db.Column(db.Integer)
-    offense_star_swings = db.Column(db.Integer)
-    offense_stars_used = db.Column(db.Integer)
-    offense_stars_success = db.Column(db.Integer)
-    offense_star_chances = db.Column(db.Integer)
-    offense_star_chances_won  = db.Column(db.Integer)#Calculated
-    innings_pitched = db.Column(db.Integer)
-    batters_faced = db.Column(db.Integer)
-    strikeouts = db.Column(db.Integer)
-    runs_allowed = db.Column(db.Integer)
-    era = db.Column(db.Float) #Calculated
-    defensive_star_pitches = db.Column(db.Integer)
-    defensive_stars_used = db.Column(db.Integer)
-    defensive_star_success = db.Column(db.Integer)
-    defensive_star_chances = db.Column(db.Integer)
-    defensive_star_chance_won  = db.Column(db.Integer) #Calculated
-
-#TODO class CharacterInfo(db.Models): #Transform stat sheet into database table
+  def __init__(self,
+    id,
+    date_time,
+    ranked,
+    stadium_id,
+    # away_player_id,
+    # home_player_id,
+    away_score,
+    home_score,
+    innings_selected,
+    innings_played,
+    home_captain,
+    home_roster_0,
+    home_roster_1,
+    home_roster_2,
+    home_roster_3,
+    home_roster_4,
+    home_roster_5,
+    home_roster_6,
+    home_roster_7,
+    home_roster_8,
+    away_captain,
+    away_roster_0,
+    away_roster_1,
+    away_roster_2,
+    away_roster_3,
+    away_roster_4,
+    away_roster_5,
+    away_roster_6,
+    away_roster_7,
+    away_roster_8,
+    quitter
+  ):
+    self.id = id
+    self.date_time = date_time
+    self.ranked = ranked
+    self.stadium_id = stadium_id
+    # self.away_player_id = away_player_id
+    # self.home_player_id = home_player_id
+    self.away_score = away_score
+    self.home_score = home_score
+    self.innings_selected = innings_selected
+    self.innings_played = innings_played
+    self.home_captain = home_captain
+    self.home_roster_0 = home_roster_0
+    self.home_roster_1 = home_roster_1
+    self.home_roster_2 = home_roster_2
+    self.home_roster_3 = home_roster_3
+    self.home_roster_4 = home_roster_4
+    self.home_roster_5 = home_roster_5
+    self.home_roster_6 = home_roster_6
+    self.home_roster_7 = home_roster_7
+    self.home_roster_8 = home_roster_8
+    self.away_captain = away_captain
+    self.away_roster_0 = away_roster_0
+    self.away_roster_1 = away_roster_1
+    self.away_roster_2 = away_roster_2
+    self.away_roster_3 = away_roster_3
+    self.away_roster_4 = away_roster_4
+    self.away_roster_5 = away_roster_5
+    self.away_roster_6 = away_roster_6
+    self.away_roster_7 = away_roster_7
+    self.away_roster_8 = away_roster_8
+    self.quitter = quitter
 
 
-# ===== Marshamllow Schemas =====
-class UserSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = User
 
-class GameSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = Game
+# ===== Schema =====
+class GameSchema(ma.Schema):
+  class Meta:
+    fields = (
+      'id',
+      'date_time',
+      'ranked',
+      'stadium_id',
+      # 'away_player_id',
+      # 'home_player_id',
+      'away_score',
+      'home_score',
+      'innings_selected',
+      'innings_played',
+      'home_captain',
+      'home_roster_0',
+      'home_roster_1',
+      'home_roster_2',
+      'home_roster_3',
+      'home_roster_4',
+      'home_roster_5',
+      'home_roster_6',
+      'home_roster_7',
+      'home_roster_8',
+      'away_captain',
+      'away_roster_0',
+      'away_roster_1',
+      'away_roster_2',
+      'away_roster_3',
+      'away_roster_4',
+      'away_roster_5',
+      'away_roster_6',
+      'away_roster_7',
+      'away_roster_8',
+      'quitter',
+    )
 
-class GameCharacterSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = GameCharacter
 
-class PitchSummarySchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = PitchSummary
-
-class ContactSummarySchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = ContactSummary
-
-class FieldingSummarySchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = FieldingSummary
-
-class UserCharacterStatsSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = UserCharacterStats
-
-#TODO add CharacterInfo model
-#class CharacterInfoSchema(ma.SQLAlchemyAutoSchema):
-#    class Meta:
-#        model = CharacterInfo
-
+game_schema = GameSchema()
+games_schema = GameSchema(many=True)
 
 # ===== API Routes =====
+@app.route('/')
+def index():
+    return 'API online...'
+
+@app.route('/game/', methods=['POST'])
+def add_game():
+    id = request.json['GameID']
+    date_time = request.json['Date']
+    ranked = request.json['Ranked']
+    stadium_id = request.json['StadiumID']
+    away_score = request.json['Away Score']
+    home_score = request.json['Home Score']
+    innings_selected = request.json['Innings Selected']
+    innings_played = request.json['Innings Played']
+    home_captain = request.json['Home Team Captain']
+    home_roster_0 = request.json['Home Team Roster'][0]
+    home_roster_1 = request.json['Home Team Roster'][1]
+    home_roster_2 = request.json['Home Team Roster'][2]
+    home_roster_3 = request.json['Home Team Roster'][3]
+    home_roster_4 = request.json['Home Team Roster'][4]
+    home_roster_5 = request.json['Home Team Roster'][5]
+    home_roster_6 = request.json['Home Team Roster'][6]
+    home_roster_7 = request.json['Home Team Roster'][7]
+    home_roster_8 = request.json['Home Team Roster'][8]
+    away_captain = request.json['Away Team Captain']
+    away_roster_0 = request.json['Away Team Roster'][0]
+    away_roster_1 = request.json['Away Team Roster'][1]
+    away_roster_2 = request.json['Away Team Roster'][2]
+    away_roster_3 = request.json['Away Team Roster'][3]
+    away_roster_4 = request.json['Away Team Roster'][4]
+    away_roster_5 = request.json['Away Team Roster'][5]
+    away_roster_6 = request.json['Away Team Roster'][6]
+    away_roster_7 = request.json['Away Team Roster'][7]
+    away_roster_8 = request.json['Away Team Roster'][8]
+    quitter = request.json['Quitter Team']
+
+    game = Game(
+      id,
+      date_time,
+      ranked,
+      stadium_id,
+      # away_player_id,
+      # home_player_id,
+      away_score,
+      home_score,
+      innings_selected,
+      innings_played,
+      home_captain,
+      home_roster_0,
+      home_roster_1,
+      home_roster_2,
+      home_roster_3,
+      home_roster_4,
+      home_roster_5,
+      home_roster_6,
+      home_roster_7,
+      home_roster_8,
+      away_captain,
+      away_roster_0,
+      away_roster_1,
+      away_roster_2,
+      away_roster_3,
+      away_roster_4,
+      away_roster_5,
+      away_roster_6,
+      away_roster_7,
+      away_roster_8,
+      quitter,
+    )
+
+    db.session.add(game)
+    db.session.commit()
+
+    return game_schema.jsonify(game)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
