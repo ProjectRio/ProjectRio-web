d5f2a004cbbe8c8fcc92dc936f4a209c962617f7
Jonathan Lui <jonslui@gmail.com>
2022-03-19 09:37:39 -0400

    started refactoring populate_db to utalize new JSON format

--- app/views/populate_db.py
+++ app/views/populate_db.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify, abort
 from flask import current_app as app
-from ..models import db, User, Character, Game, CharacterGameSummary, PitchSummary, ContactSummary, FieldingSummary, ChemistryTable, Tag, GameTag
+from ..models import db, User, Character, Game, CharacterGameSummary, CharacterPositionSummary, Event, Runner, PitchSummary, ContactSummary, FieldingSummary, ChemistryTable, Tag, GameTag
 import json
 from ..consts import *
 
@@ -266,7 +266,7 @@ def populate_db():
 def populate_db2():
     tags = []
 
-    # Boolean to check if a game has a superstar player
+    # Boolean used to assign a GameTag after creating CharacterGameSummary rows
     is_superstar_game = False
     
     # Get players from db User table
@@ -301,4 +301,282 @@ def populate_db2():
     db.session.add(game)
     db.session.commit()
 
-    return 'completed...'
+
+    # ======= Character Game Summary =======
+    teams = {
+        'Home': [None] * 9,
+        'Away': [None] * 9,
+    }
+    character_game_stats = request.json['Character Game Stats']
+    characters = [character_game_stats['Team 0 Roster 0'], character_game_stats['Team 0 Roster 1'], character_game_stats['Team 0 Roster 2'], character_game_stats['Team 0 Roster 3'], character_game_stats['Team 0 Roster 4'], character_game_stats['Team 0 Roster 5'], character_game_stats['Team 0 Roster 6'], character_game_stats['Team 0 Roster 7'], character_game_stats['Team 0 Roster 8'], character_game_stats['Team 1 Roster 0'], character_game_stats['Team 1 Roster 1'], character_game_stats['Team 1 Roster 2'], character_game_stats['Team 1 Roster 3'], character_game_stats['Team 1 Roster 4'], character_game_stats['Team 1 Roster 5'], character_game_stats['Team 1 Roster 6'], character_game_stats['Team 1 Roster 7'], character_game_stats['Team 1 Roster 8']]    
+    for character in characters:
+        pitches_per_position = character['Defensive Stats']['Pitches Per Position'] if len(character['Defensive Stats']['Pitches Per Position']) == 1 else [{}]
+        outs_per_position = character['Defensive Stats']['Outs Per Position'] if len(character['Defensive Stats']['Outs Per Position']) == 1 else [{}]
+
+        character_position_summary = CharacterPositionSummary(
+            pitches_at_p = 0 if 'P' not in pitches_per_position[0] else pitches_per_position[0]['P'],
+            pitches_at_c = 0 if 'C' not in pitches_per_position[0] else pitches_per_position[0]['C'],
+            pitches_at_1b = 0 if '1B' not in pitches_per_position[0] else pitches_per_position[0]['1B'],
+            pitches_at_2b = 0 if '2B' not in pitches_per_position[0] else pitches_per_position[0]['2B'],
+            pitches_at_3b = 0 if '3B' not in pitches_per_position[0] else pitches_per_position[0]['3B'],
+            pitches_at_ss = 0 if 'SS' not in pitches_per_position[0] else pitches_per_position[0]['SS'],
+            pitches_at_lf = 0 if 'LF' not in pitches_per_position[0] else pitches_per_position[0]['LF'],
+            pitches_at_cf = 0 if 'CF' not in pitches_per_position[0] else pitches_per_position[0]['CF'],
+            pitches_at_rf = 0 if 'RF' not in pitches_per_position[0] else pitches_per_position[0]['RF'],
+            outs_at_p = 0 if 'P' not in outs_per_position[0] else outs_per_position[0]['P'],
+            outs_at_c = 0 if 'C' not in outs_per_position[0] else outs_per_position[0]['C'],
+            outs_at_1b = 0 if '1B' not in outs_per_position[0] else outs_per_position[0]['1B'],
+            outs_at_2b = 0 if '2B' not in outs_per_position[0] else outs_per_position[0]['2B'],
+            outs_at_3b = 0 if '3B' not in outs_per_position[0] else outs_per_position[0]['3B'],
+            outs_at_ss = 0 if 'SS' not in outs_per_position[0] else outs_per_position[0]['SS'],
+            outs_at_lf = 0 if 'LF' not in outs_per_position[0] else outs_per_position[0]['LF'],
+            outs_at_cf = 0 if 'CF' not in outs_per_position[0] else outs_per_position[0]['CF'],
+            outs_at_rf = 0 if 'RF' not in outs_per_position[0] else outs_per_position[0]['RF'],
+        )
+
+        db.session.add(character_position_summary)
+        db.session.commit()
+
+        defensive_stats = character['Defensive Stats']
+        offensive_stats = character['Offensive Stats']
+
+        character_game_summary = CharacterGameSummary(
+            game_id = game.game_id,
+            team_id = int(character['Team']),
+            char_id = character['CharID'],
+            user_id = home_player.id if character['Team'] == '0' else away_player.id,
+            character_position_summary_id = character_position_summary.id,
+            roster_loc = character['RosterID'],
+            captain = character['Captain'],
+            superstar = character['Superstar'],
+            fielding_hand = character['Fielding Hand'],
+            batting_hand = character['Batting Hand'],
+            # Defensive Stats
+            batters_faced = defensive_stats['Batters Faced'],
+            runs_allowed = defensive_stats['Runs Allowed'],
+            batters_walked = defensive_stats['Batters Walked'],
+            batters_hit = defensive_stats['Batters Hit'],
+            hits_allowed = defensive_stats['Hits Allowed'],
+            homeruns_allowed = defensive_stats['HRs Allowed'],
+            pitches_thrown = defensive_stats['Pitches Thrown'],
+            stamina = defensive_stats['Stamina'],
+            # Should this be a boolean
+            was_pitcher = defensive_stats['Was Pitcher'],
+            strikeouts_pitched = defensive_stats['Strikeouts'],
+            star_pitches_thrown = defensive_stats['Star Pitches Thrown'],
+            big_plays = defensive_stats['Big Plays'],
+            outs_pitched = defensive_stats['Outs Pitched'],
+            # Offensive Stats
+            at_bats = offensive_stats['At Bats'],
+            hits = offensive_stats['Hits'],
+            singles = offensive_stats['Singles'],
+            doubles = offensive_stats['Doubles'],
+            triples = offensive_stats['Triples'],
+            homeruns = offensive_stats['Homeruns'],
+            successful_bunts = offensive_stats['Successful Bunts'],
+            sac_flys = offensive_stats['Sac Flys'],
+            strikeouts = offensive_stats['Strikeouts'],
+            walks_bb = offensive_stats['Walks (4 Balls)'],
+            walks_hit = offensive_stats['Walks (Hit)'],
+            rbi = offensive_stats['RBI'],
+            bases_stolen = offensive_stats['Bases Stolen'],
+            star_hits = offensive_stats['Star Hits'],
+            #Star tracking (Not in JSON. Calculated in populate_db)
+            offensive_star_swings = 0,
+            offensive_stars_used = 0,
+            offensive_stars_put_in_play = 0,
+            offensive_star_successes = 0,
+            offensive_star_chances = 0,
+            offensive_star_chances_won = 0,
+            defensive_star_pitches = 0,
+            defensive_stars_used = 0,
+            defensive_star_successes = 0,
+            defensive_star_chances = 0,
+            defensive_star_chances_won = 0
+        )
+
+        db.session.add(character_game_summary)
+        db.session.commit()
+
+        # index character_game_summarys for later use
+        if character['Team'] == '0':
+            teams['Home'][character['RosterID']] = character_game_summary
+        else:
+            teams['Away'][character['RosterID']] = character_game_summary
+
+        # ==== GameTag ====
+        if character['Superstar'] == 1:
+            is_superstar_game = True 
+
+    if game.ranked == True:
+        tags.append('Ranked')
+    else:
+        tags.append('Unranked')
+
+    if is_superstar_game == True:
+        tags.append('Superstar')
+    else:
+        tags.append('Normal')
+    
+    for name in tags:
+        tag = Tag.query.filter_by(name=name).first()
+        if tag:
+            game_tag = GameTag(
+                game_id = game.game_id,
+                tag_id = tag.id
+            )
+            db.session.add(game_tag)
+
+
+# Create Events, Runners, PitchSummaries, ContactSummaries, and FieldingSummaries
+    # contains json data for comparing events
+    previous_runners_json = {
+        'Runner Batter': None,
+        'Runner 1B': None,
+        'Runner 2B': None,
+        'Runner 3B': None
+    }
+    # contains model instances for use if current event data equal previous event data
+    previous_runners = {
+        'Runner Batter': None,
+        'Runner 1B': None,
+        'Runner 2B': None,
+        'Runner 3B': None
+    }
+    events = request.json['Events']
+    for index, event_data in enumerate(events):
+        # ======= Create Event rows ======
+        event = Event(
+            game_id = game.game_id,
+            pitcher_id = teams['Home'][event_data['Pitcher Roster Loc']].id if event_data['Half Inning'] == 0 else teams['Away'][event_data['Pitcher Roster Loc']].id,
+            catcher_id = teams['Home'][event_data['Catcher Roster Loc']].id if event_data['Half Inning'] == 0 else teams['Away'][event_data['Catcher Roster Loc']].id,
+            event_num = index,
+            away_score = event_data['Away Score'],
+            home_score = event_data['Home Score'],
+            inning = event_data['Inning'],
+            half_inning = event_data['Half Inning'],
+            chem_links_ob = event_data['Chemistry Links on Base'],
+            star_chance = event_data['Star Chance'],
+            away_stars = event_data['Away Stars'],
+            home_stars = event_data['Home Stars'],
+            pitcher_stamina = event_data['Pitcher Stamina'],
+            outs = event_data['Outs'],
+            balls = event_data['Balls'],
+            strikes = event_data['Strikes'],
+            result_rbi = event_data['RBI'],
+            result_of_ab = event_data['Result of AB'],
+
+        )
+
+        # ======= Create Runner rows for batters ======
+        # Loop through the four possible json event runner keys, check if their values are equal to the values from the previous event (this means they are the same character), and then use previous runner row or create a new one accordingly.
+        for key in previous_runners:
+            if key in event_data:
+                if previous_runners_json[key] and previous_runners_json[key] == event_data[key]:
+                    if key == 'Runner Batter':
+                        event.runner_on_0 = previous_runners[key].id
+                    elif key == 'Runner 1B':
+                        event.runner_on_1 = previous_runners[key].id
+                    elif key == 'Runner 2B':
+                        event.runner_on_2 = previous_runners[key].id
+                    elif key == 'Runner 3B':
+                        event.runner_on_3 = previous_runners[key].id
+                else:
+                    runner_batter = Runner(
+                        runner_character_game_summary_id = teams['Away'][event_data[key]['Runner Roster Loc']].id if event_data['Half Inning'] == 0 else teams['Home'][event_data[key]['Runner Roster Loc']].id,
+                        initial_base = event_data[key]['Runner Initial Base'],
+                        result_base = event_data[key]['Runner Result Base'],
+                        out_type = event_data[key]['Out Type'],
+                        out_location = event_data[key]['Out Location'],
+                        steal = event_data[key]['Steal'],                   
+                    )
+
+                    db.session.add(runner_batter)
+                    db.session.commit()
+
+                    if key == 'Runner Batter':
+                        event.runner_on_0 = runner_batter.id
+                    elif key == 'Runner 1B':
+                        event.runner_on_1 = runner_batter.id
+                    elif key == 'Runner 2B':
+                        event.runner_on_2 = runner_batter.id
+                    elif key == 'Runner 3B':
+                        event.runner_on_3 = runner_batter.id
+
+                    previous_runners[key] = runner_batter
+                    previous_runners_json[key] = event_data[key]
+            else:
+                previous_runners['Runner Batter'] = None
+                previous_runners_json['Runner Batter'] = None
+
+            
+        # ==== Pitch Summary ====
+        pitch_summary = PitchSummary(
+            batter_id = event.runner_on_0,
+            pitcher_id = event.pitcher_id,
+            pitch_type = event_data['Pitch']['Pitch Type'],
+            charge_pitch_type = event_data['Pitch']['Charge Type'],
+            star_pitch = event_data['Pitch']['Star Pitch'],
+            pitch_speed = event_data['Pitch']['Pitch Speed'],
+            pitch_result = event_data['Pitch']['Pitch Result'],
+            type_of_swing = event_data['Pitch']['Type of Swing']
+        )
+
+        # if the batter made contact with the pitch
+        if 'Contact' in event_data['Pitch']:
+            #  ==== Contact Summary ====
+            contact_summary = ContactSummary(
+                type_of_contact = event_data['Pitch']['Contact']['Type of Contact'],
+                charge_power_up = event_data['Pitch']['Contact']['Charge Power Up'],
+                charge_power_down = event_data['Pitch']['Contact']['Charge Power Down'],
+                star_swing_five_star = event_data['Pitch']['Contact']['Star Swing Five-Star'],
+                input_direction = event_data['Pitch']['Contact']['Input Direction'],
+                frame_of_swing_upon_contact = event_data['Pitch']['Contact']['Frame Of Swing Upon Contact'],
+                ball_angle = event_data['Pitch']['Contact']['Ball Angle'],
+                ball_horiz_power = event_data['Pitch']['Contact']['Ball Horizontal Power'],
+                ball_vert_power = event_data['Pitch']['Contact']['Ball Vertical Power'],
+                ball_x_velocity = event_data['Pitch']['Contact']['Ball Velocity - X'],
+                ball_y_velocity = event_data['Pitch']['Contact']['Ball Velocity - Y'],
+                ball_z_velocity = event_data['Pitch']['Contact']['Ball Velocity - Z'],
+                ball_x_pos = event_data['Pitch']['Contact']['Ball Landing Position - X'],
+                ball_y_pos = event_data['Pitch']['Contact']['Ball Landing Position - Y'],
+                ball_z_pos = event_data['Pitch']['Contact']['Ball Landing Position - Z'],
+                ball_x_pos_upon_hit = event_data['Pitch']['Contact']['Ball Position Upon Contact - X'],
+                ball_z_pos_upon_hit = event_data['Pitch']['Contact']['Ball Position Upon Contact - Z'],
+                batter_x_pos_upon_hit = event_data['Pitch']['Contact']['Batter Position Upon Contact - X'],
+                batter_z_pos_upon_hit = event_data['Pitch']['Contact']['Batter Position Upon Contact - Z'],
+                multi_out = event_data['Pitch']['Contact']['Multi-out'],
+                primary_result = event_data['Pitch']['Contact']['Contact Result - Primary'],
+                secondary_result = event_data['Pitch']['Contact']['Contact Result - Secondary']
+            )
+
+            db.session.add(contact_summary)
+            db.session.commit()
+            pitch_summary.contact_summary_id = contact_summary.id
+
+            # ==== Fielding Summary ====
+            if 'First Fielder' in event_data['Pitch']['Contact']:
+                fielder_data = event_data['Pitch']['Contact']['First Fielder']
+
+                fielding_summary = FieldingSummary(
+                    fielder_character_game_summary_id = teams['Home'][fielder_data['Fielder Roster Location']].id if event_data['Half Inning'] == 0 else teams['Away'][fielder_data['Fielder Roster Location']].id,
+                    position = fielder_data['Fielder Position'],
+                    action = fielder_data['Fielder Action'],
+                    bobble = fielder_data['Fielder Bobble'],
+                    swap = fielder_data['Fielder Swap'],
+                    fielder_x_pos = fielder_data['Fielder Position - X'],
+                    fielder_y_pos = fielder_data['Fielder Position - Y'],
+                    fielder_z_pos = fielder_data['Fielder Position - Z']
+                )
+
+                db.session.add(fielding_summary)
+                db.session.commit()
+                contact_summary.fielding_summary_id = fielding_summary.id
+                db.session.add(contact_summary)
+            
+        db.session.add(pitch_summary)
+        event.pitch_summary_id = pitch_summary.id
+        db.session.add(event)
+        db.session.commit()
+
+    return 'Completed...'
