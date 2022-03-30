59c01f390de0ff2df5d3f376f687b255857f110c
Jonathan Lui <jonslui@gmail.com>
2022-03-30 17:39:58 -0400

    script now changes username to DemoUser

--- populate-db-script.py
+++ populate-db-script.py
@@ -47,18 +47,28 @@ for user in users["Users"]:
 print('Uploading Game data:')
 for file_path in os.listdir("./json/games/"):
     if file_path != '.DS_Store':
+        editted_file = False
         with open("./json/games/" + file_path, 'r') as f:
             game_data = json.load(f)
 
-            # Replace players with demo players
+            # Check if JSON file has replaced player names, if not, replace them.
             demo_players = random.sample(users["Users"], 2)
-            away_player = demo_players[0]['Username']
-            home_player = demo_players[1]['Username']
-            game_data['Away Player'] = away_player
-            game_data['Home Player'] = home_player
+            away_player = game_data['Away Player'] 
+            home_player = game_data['Home Player']
 
-        with open("./json/games/" + file_path, 'w') as f:
-            f.write(json.dumps(game_data, indent=4))
+            if game_data['Away Player'][0:8] != 'DemoUser':
+                game_data['Away Player'] = demo_players[0]['Username']
+                away_player = demo_players[0]['Username']
+                editted_file = True
+
+            if game_data['Home Player'][0:8] != 'DemoUser':
+                game_data['Home Player'] = demo_players[1]['Username']
+                home_player = demo_players[1]['Username']
+                eddited_file = True
+
+        if editted_file == True:
+            with open("./json/games/" + file_path, 'w') as f:
+                f.write(json.dumps(game_data, indent=4))
 
         print(file_path + ': ' + game_data["GameID"] + ' Players: ' + away_player + ' vs ' + home_player)
 
