b56bf40ccc03ea8357b9e25533b2b146e6b9e80c
Connor <connornace13@gmail.com>
2022-04-15 23:22:58 -0400

    Adding warn to logs

--- app/views/log.py
+++ app/views/log.py
@@ -1,12 +1,23 @@
 from flask import current_app as app
 from flask import request
+from datetime import datetime
 
-cLoggedEndpoints = ['endpoint_games', 'endpoint_detailed_stats']
+cLoggedEndpoints       = ['endpoint_games', 'endpoint_detailed_stats', 'user_stats', 
+                          'endpoint_batter_position', 'verify_email', 'request_password_change', 'key']
+cLoggedEndpointsNoArgs = ['register', 'change_password', 'login']
 
 @app.after_request
-def after_request_func(response):
-    print("LOG")
-    print(request.endpoint)
+def after_request_func(response):    
+    now = datetime.now()
+    # dd/mm/YY H:M:S
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
     if (request.endpoint in cLoggedEndpoints):
-        app.logger.info(f'Datatime: {response.date}  Endpoint: {request.endpoint}  RC: {response.status}  IP: {request.remote_addr}  Args: {request.args} ')
+        app.logger.info(f'Datatime: {dt_string}  Endpoint: {request.endpoint}  RC: {response.status}  IP: {request.remote_addr}  Args: {request.args} ')
+    if (request.endpoint in cLoggedEndpointsNoArgs):
+        app.logger.info(f'Datatime: {dt_string}  Endpoint: {request.endpoint}  RC: {response.status}  IP: {request.remote_addr} ')
+    
+    #If HTTP request was not successful for any reason log a warning
+    if (response.status_code > 300):
+        app.logger.warning(f'Datatime: {dt_string}  Endpoint: {request.endpoint}  RC: {response.status}  IP: {request.remote_addr} ')
     return response
