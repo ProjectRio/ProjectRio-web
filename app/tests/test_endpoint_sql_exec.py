903aa038014dedf628f7852687c7ab4a80aeb046
CNace13 <connornace13@gmail.com>
2022-08-20 15:31:19 -0400

    Beginning of tests for sql exec

---
+++ app/tests/test_endpoint_sql_exec.py
@@ -0,0 +1,10 @@
+import requests
+from connection import Connection
+
+db = Connection()
+
+# External tests
+def test_external_endpoint_gen_woba_data():
+    response = requests.post("http://127.0.0.1:5000/sql_test/")
+    print(response)
+    assert response.status_code == 200
