e06ec82e42d2ccf153e761eaa271b8f6ec2ebd95
CNace13 <connornace13@gmail.com>
2022-08-20 14:35:03 -0400

    Slight rework of tags

--- app/views/tag.py
+++ app/views/tag.py
@@ -12,46 +12,41 @@ import time
 @jwt_required(optional=True)
 def tag_create():
     in_tag_name = request.json['Tag Name']
-    in_tag_type = request.json['Tag Type']
     in_tag_desc = request.json['Description']
+    in_tag_comm_name = request.json['Community Name']
 
-    comm = None
-    try:
-        in_tag_comm_name = request.json['Community Name']
-        comm_name_lower = in_tag_comm_name.lower()
-        comm = Community.query.filter_by(name_lowercase=comm_name_lower).first()
+    comm_name_lower = in_tag_comm_name.lower()
+    comm = Community.query.filter_by(name_lowercase=comm_name_lower).first()
 
-        if comm == None:
-            return abort(409, "No community found with name={in_tag_comm_name}")
-    except:
-        print("No community name given, assuming this is a public tag")
+    if comm == None:
+        return abort(409, "No community found with name={in_tag_comm_name}")
 
     # Get user making the new community
-    #Get user via JWT or RioKey 
+    #Get user via JWT or RioKey
     user=None
-    try:
-        current_user_username = get_jwt_identity()
+    current_user_username = get_jwt_identity()
+    if current_user_username:
         user = RioUser.query.filter_by(username=current_user_username).first()
-    except:
-        user = RioUser.query.filter_by(rio_key=request.json['Rio Key']).first()
+    else:
+        try:
+            user = RioUser.query.filter_by(rio_key=request.json['Rio Key']).first()
+        except:
+            return abort(409, "No Rio Key or JWT Provided")
 
     if user == None:
-        return abort(409, description='Username associated with JWT or RioKey not found.')
+        return abort(409, description='Username associated with JWT not found.')
     
     #If community tag, make sure user is an admin of the community
     comm_user = CommunityUser.query.filter_by(user_id=user.id, community_id=comm.id).first()
 
-    if (comm != None and (comm_user == None or comm_user.admin == False) ):
+    if (comm_user == None or comm_user.admin == False):
         return abort(409, description='User not apart of community or not an admin')
-    if (comm == None and user.username_lower not in ['peacockslayer', 'maybejon', 'littlecoaks']): #TODO eventually use user groups for this
-        return abort(403, description='Public tag is trying to be created but user is not authorized to do so')
 
     # === Tag Creation ===
-    comm_id = None if comm == None else comm.id
-    new_tag = Tag( in_comm_id=comm_id, in_tag_name=in_tag_name, in_tag_type=in_tag_type, in_desc=in_tag_desc)
+    new_tag = Tag( in_comm_id=comm.id, in_tag_name=in_tag_name, in_tag_type="Component", in_desc=in_tag_desc)
     db.session.add(new_tag)
     db.session.commit()
 
     #TODO this might not work, but its late and I gotta end this
     #"this" meaning the coding session
-    return jsonify(new_tag._asdict())
+    return jsonify(new_tag.name)
