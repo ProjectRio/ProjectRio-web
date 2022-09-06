7a4158f603028833b9c6c32dc0c1a1a8eb47f3c8
Jonathan Lui <jonslui@gmail.com>
2022-09-06 15:42:25 -0400

    adjusted send_email function to only send in production

--- app/email.py
+++ app/email.py
@@ -1,19 +1,21 @@
 import os
-from flask import abort as kill
+from flask import abort as kill, current_app as app
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail, From
 
 def send_email(to_email, subject, html_content):
-    message = Mail(
-        from_email=From('email@projectrio.app', 'Rio Web'),
-        to_emails=to_email,
-        subject=subject,
-        html_content=html_content
-    )
-    try:
-        sg = SendGridAPIClient(os.getenv("SEND_GRID_KEY"))
-        response = sg.send(message)
-    except:
-        kill(502, 'Failed to send email')
+    # If the application is running in production, send emails using sendgrid.
+    if (app.env == "production"):
+        message = Mail(
+            from_email=From('email@projectrio.app', 'Rio Web'),
+            to_emails=to_email,
+            subject=subject,
+            html_content=html_content
+        )
+        try:
+            sg = SendGridAPIClient(os.getenv("SEND_GRID_KEY"))
+            response = sg.send(message)
+        except:
+            kill(502, 'Failed to send email')
 
     return "Email sent..."
