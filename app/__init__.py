eae1ea040782eefbde0c3d2e4c7cc2cd7daf24f8
CNace13 <connornace13@gmail.com>
2024-06-17 00:19:01 -0400

    Re-adding __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,76 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+import logging
+from pathlib import Path
+from logging.handlers import RotatingFileHandler
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+
+# Globally accessible libraries
+db = SQLAlchemy()
+bc = Bcrypt()
+jwt = JWTManager()
+sched = BackgroundScheduler(daemon=True)
+migrate = Migrate()
+
+def init_app():
+    # Construct core application
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    app.config['rio_env'] = os.getenv('RIO_ENV')
+    # app.config['SQLALCHEMY_ECHO'] = True
+
+    # Define the folder where you want to save the JSON files
+    UPLOAD_FOLDER = 'games'
+    UPLOAD_PATH = Path(__file__).resolve().parent / UPLOAD_FOLDER
+    app.config['GAMES_UPLOAD_FOLDER'] = UPLOAD_PATH
+
+    CORS(app)
+
+    # Initialize Plugins
+    db.init_app(app)
+    bc.init_app(app)
+    jwt.init_app(app)
+    migrate.init_app(app, db)
+
+    #Set logger properties
+    #Rotating log file
+    handler = RotatingFileHandler('endpoint_log.log', maxBytes=10000000, backupCount=2) #10 MB file size
+    handler.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
+
+    with app.app_context():
+        #import routes
+        from .views import populate_db
+        from .views import user
+        from .views import db_setup
+        from .views import delete_game
+        from .views import recreate_stat_files
+        from .views import client_routes
+        from .views import stat_retrieval
+        from .views.stats import box_score
+        from .views.stats import user_summary
+        from .views import api_key
+        from .views import user_groups
+        from .views import community
+        from .views import tag
+        from .views import sql_exec
+        from .views import reverify_emails
+        from .views import db_manage
+        # from .views import log
+
+        # Create a cron trigger for running every minute
+        min_trigger = CronTrigger(year="*", month="*", day="*", hour="*", minute="*", second="0")
+        # Process games every minute on the minute
+        sched.add_job(populate_db.process_all_games_job, trigger=min_trigger, args=[app])
+        sched.start()
+            
+        #create sql tables for data models
+        db.create_all()
+
+        return app
